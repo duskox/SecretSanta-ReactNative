40e656dea62e08631ee1ed3a57f9c694d49275bd
Dusan Vorkapic <dvor@FL229-Dusko.local>
2017-10-01 22:20:19 +0200

    Remove not needed code

--- src/component/Register.js
+++ src/component/Register.js
@@ -15,16 +15,9 @@ import styles from '../../style/styles';
 import Button from 'react-native-button';
 import Icon from 'react-native-vector-icons/Entypo';
 
-function MyButton(props) {
-  return (
-    <Button style={styles.googleButtonStyle}>
-      <Icon.Button name = "google--with-circle" backgroundColor="#d34836" size={80} >
-        <Text style = {{fontSize: 15, color: 'white'}}>Login with Google</Text>
-      </Icon.Button>
-    </Button>
-  );
+function onLoginWithGooglePress() {
+  console.log("BLA!");
 }
-
 
 export default class Register extends React.Component {
   constructor(props) {
@@ -41,7 +34,6 @@ export default class Register extends React.Component {
     return (
       <View style={ [styles.container, {height: this.state.visibleHeight}] } >
         <Image source={require('../../assets/santa.jpg')} resizeMode='contain' style={styles.imageItem}/>
-        <MyButton onPress=''/>
       </View>
     );
   }
