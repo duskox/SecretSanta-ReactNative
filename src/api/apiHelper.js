8c4fd5337157b947fa9359199e0dad8017450d3a
Dusan Vorkapic <dusan.vorkapic@futurice.com>
2017-12-02 02:00:05 +0100

    Remove commented out code.

--- src/api/apiHelper.js
+++ src/api/apiHelper.js
@@ -3,36 +3,15 @@ import axios from 'axios'
 
 export function setUser(data) {
   const url = BACKEND_URL + 'santApi/setuser';
-  // return fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data)
-  // })
   return axios.post(url, data)
 }
 
 export function joinRaffle(data) {
   const url = BACKEND_URL + 'santApi/join';
-  // return fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data)
-  // })
   return axios.post(url, data)
 }
 
 export function leaveRaffle(data) {
   const url = BACKEND_URL + 'santApi/leave';
-  // return fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data)
-  // })
   return axios.post(url, data)
 }
